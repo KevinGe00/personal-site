7c78b11eca0597eb2fe02540964876c2eef37eb5
Kevin Ge <kevin.ge@mail.utoronto.ca>
2020-01-19 23:08:30 -0500

    updated background gradient, updated project section

--- src/components/aboutComponent.js
+++ src/components/aboutComponent.js
@@ -9,12 +9,12 @@ class AboutComponent extends Component {
                 <h1 class="center-text font-inherit title-margin">About me</h1>
                 <div class="row">
                     <div class="col-sm-6">
-                        <p id="about-text"><strong>Nice to meet you, I'm Kevin!</strong>
+                        <p id="about-text"><strong>Nice to meet you 👋, I'm Kevin!</strong>
                             <hr></hr>
                             I'm currently a second year computer science student at the Univeristy of Toronto.
                             When I'm not spending time on school, I'll be with friends, working out or spending time
                             curating my <a href="https://open.spotify.com/user/citatlon">Spotify playlists</a>. I built 
-                            this website as way for people to get to know me better and to find out what I've done. So please, feel free 
+                            this website (in React<i class="fab fa-react"></i>) as way for people to get to know me better and to find out what I've done. So please, feel free 
                             to check out my projects below.
                             
                         </p>
