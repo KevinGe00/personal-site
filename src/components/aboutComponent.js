dc48a1dcf533e35fefc74818098db8093dfc7dd9
Kevin Ge <kevin.ge@mail.utoronto.ca>
2020-01-28 22:55:34 -0500

    typo

--- src/components/aboutComponent.js
+++ src/components/aboutComponent.js
@@ -14,7 +14,7 @@ class AboutComponent extends Component {
                             I'm currently a second year computer science student at the University of Toronto.
                             When I'm not spending time on school, I'll be with friends, working out or spending time
                             curating my <a href="https://open.spotify.com/user/citatlon">Spotify playlists</a>. I built 
-                            this website (in React<i class="fab fa-react"></i>) as way for people to get to know me better and to find out what I've done. So please, feel free 
+                            this website (in React<i class="fab fa-react"></i>) as a way for people to get to know me better and to find out what I've done. So please, feel free 
                             to check out my projects below.
                         </p>
                     </div>
